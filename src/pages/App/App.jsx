4fee323e1e45b41f5e34e2d30ab622cfc981e84a
btlenny <btlenny@gmail.com>
2023-12-23 12:26:15 -0800

    adding photo grid

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import NavBar from '../../components/NavBar'
+import NavBar from '../../components/NavBar/NavBar'
+import Gallery from '../../components/Gallery/Gallery'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
     <>
       <main className="App">
         <NavBar />
-
+        <Gallery />
 
       </main>
     </>
