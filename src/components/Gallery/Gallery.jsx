5b0a004c267c2faa7905d3aeb8d9eac7261581e8
btlenny <btlenny@gmail.com>
2023-12-28 11:26:57 -0800

    fade in working

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,51 +1,53 @@
-
+import './Gallery.css';
 
 export default function Gallery() {
     return (
    <> 
+
+   
 <div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
     <div class="grid gap-4">
         <div>
             <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo1.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo2.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo2.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo3.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo4.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo5.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo6.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo3.jpg" alt=""/>
         </div>
     </div>
     <div class="grid gap-4">
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo7.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo4.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo8.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo5.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo9.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo6.jpg" alt=""/>
         </div>
     </div>
     <div class="grid gap-4">
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo10.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo7.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo11.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo8.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xs" src="/images/Photo12.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo9.jpg" alt=""/>
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo10.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo11.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo12.jpg" alt=""/>
         </div>
     </div>
 </div>
