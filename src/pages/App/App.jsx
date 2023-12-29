51d84341cd8668d49fd1c16f2c30bd2e2d97514d
btlenny <btlenny@gmail.com>
2023-12-29 13:49:04 -0800

    setting routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import NavBar from '../../components/NavBar/NavBar'
 import Gallery from '../../components/Gallery/Gallery'
+import ContactForm from '../../components/ContactForm/ContactForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <main className="App">
-      <NavBar />
-        <Routes> 
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/" element={<Gallery />} />
-        </Routes>
-      </main>
+      <Router>
+        <main className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/contact" element={<ContactForm />} />
+            <Route path="/" element={<Gallery />} />
+          </Routes>
+        </main>
+      </Router>
     </>
-  )
+  );
 }
+
 
 export default App
