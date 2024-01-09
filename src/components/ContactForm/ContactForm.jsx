844cd9e38c3e43f16561488e99b5fc647759a684
btlenny <btlenny@gmail.com>
2024-01-08 20:58:33 -0800

    contact form working

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -36,13 +36,13 @@ export default function ContactForm() {
               htmlFor="email"
               className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              Your email
+              Your Name
             </label>
             <input
-              type="email"
-              id="email"
+              type="name"
+              name="user_name"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="John Doe"
               required
             />
           </div>
@@ -51,10 +51,25 @@ export default function ContactForm() {
               htmlFor="message"
               className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-400"
             >
-              Your message
+                Your Email
+            </label>
+            <input
+              type="email"
+              name="user_email"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="name@abc.com"
+              required
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Your Message
             </label>
             <textarea
-              id="message"
+              name="message"
               rows="6"
               className="block p-4 w-full text-xs text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Leave a comment..."
