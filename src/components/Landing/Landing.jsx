153f6aeca80ed989be938f77afc5c49c72772b05
btlenny <btlenny@gmail.com>
2024-01-10 12:20:26 -0800

    photo slides

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,17 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import './Landing.css';
 
+const imageUrls = [
+  '/images/Photo1.jpg',
+  '/images/Photo2.jpg',
+  '/images/Photo3.jpg',
+  '/images/Photo4.jpg',
+  '/images/Photo5.jpg',
+];
+
 export default function Landing() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === imageUrls.length - 1 ? 0 : prevIndex + 1
+      );
+      setImageLoaded(false); // Reset imageLoaded for each new image
+    }, 5000); // 5000 milliseconds (5 seconds)
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div className="landing-page">
       <div className="content-container">
         <div className="image-container">
-          <img src="/images/PhotoLanding.jpg" alt="Landing Page Image" />
+          <img
+            src={imageUrls[currentImageIndex]}
+            alt="Landing Page Image"
+            className={`fade-in ${imageLoaded ? 'visible' : ''}`}
+            onLoad={handleImageLoad}
+          />
         </div>
         <div className="text-container text-center">
-        <img src="/logo.svg" id="logo" className=" mx-auto" />
+          <img src="/logo.svg" id="logo" className="mx-auto" alt="Logo" />
           <h1>RAW MOMENTS CAPTURED</h1>
-         <br></br>
-          <a href="/work" className="border p-2 inline-block mt-4 rounded-sm italic text-sm">
+          <a href="/work" className="border p-2 inline-block mt-4">
             SEE WORK
           </a>
         </div>
