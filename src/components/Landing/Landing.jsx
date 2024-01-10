9bfbf5599dc31671feeaf648471c53baf0d49956
btlenny <btlenny@gmail.com>
2024-01-09 16:50:10 -0800

    updating fonts

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,18 +1,21 @@
-import  './Landing.css';
+import './Landing.css';
 
-export default function Landing(){
-    return (
-        <div className="landing-page">
-          <div className="content-container">
-            <div className="image-container">
-              <img src="/images/Photo1.jpg" alt="Landing Page Image" />
-            </div>
-            <div className="text-container">
-              <h1>Your Title</h1>
-              <p>Your description goes here.</p>
-              <a href="/work">Explore More</a>
-            </div>
-          </div>
+export default function Landing() {
+  return (
+    <div className="landing-page">
+      <div className="content-container">
+        <div className="image-container">
+          <img src="/images/Photo1.jpg" alt="Landing Page Image" />
         </div>
-      );
-    }
+        <div className="text-container text-center">
+        <img src="/logo.svg" id="logo" className=" mx-auto" />
+          <h1>RAW MOMENTS CAPTURED</h1>
+         
+          <a href="/work" className="border p-2 inline-block mt-4">
+            SEE WORK
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
