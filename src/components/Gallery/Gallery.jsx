fd7e81a13cc9b671ff35bc69c6dcc1082b887a64
btlenny <btlenny@gmail.com>
2024-01-11 11:01:28 -0800

    adding grid for photos

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,54 +1,26 @@
 import './Gallery.css';
 
 export default function Gallery() {
-    return (
-   <> 
-<div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo1.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo2.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo3.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo4.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo5.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo6.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo7.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo8.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo9.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo10.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo11.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-xs fadeInDown" src="/images/Photo12.jpg" alt=""/>
-        </div>
-    </div>
-</div>
+  return (
+    <>
+      {renderSection(["/images/Photo1.jpg", "/images/Photo2.jpg", "/images/Photo3.jpg", "/images/Photo4.jpg", "/images/Photo5.jpg", "/images/Photo6.jpg"])}
+      <hr className="w-full border-t-2 my-4" />
+      {renderSection(["/images/Photo7.jpg", "/images/Photo8.jpg", "/images/Photo9.jpg", "/images/Photo10.jpg", "/images/Photo11.jpg", "/images/Photo12.jpg"])}
+    </>
+  );
 
-</> 
-)}
+  function renderSection(imageSources) {
+    return (
+      <div className="p-4">
+        <div className="grid grid-cols-3 gap-4">
+          {imageSources.map((src, index) => (
+            <div key={index}>
+              <img className="h-auto max-w-full rounded-xs fadeInDown" src={src} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
